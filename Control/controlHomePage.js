2aba8103b959a49aea9b9224b028489c0ee2f393
sang-il Yoon <yunsangil7749@gmail.com>
2024-08-27 14:06:22 +0900

    chore : 검색 섹션 코드 수정

--- Control/controlHomePage.js
+++ Control/controlHomePage.js
@@ -1,4 +1,4 @@
-import { ApiInfo } from "./data.js";
+import { ApiInfo } from "./ApiInfo.js";
 
 //스크롤에 따라 상단 네비바 숨김 보임처리.
 const controlHeader = () => {
@@ -24,28 +24,29 @@ const controlHeader = () => {
 
 // 검색 영역의 background 이미지 불러오고 검색 영역의 배경이미지로 지정.
 const setSearchBackground = () => {
-  const base_url = ApiInfo.images.base_url;
-  const backdrop_size = ApiInfo.images.backdrop_sizes.w780;
+  const random = Math.floor(Math.random() * 20);
 
   const settings = {
     async: true,
     crossDomain: true,
-    url: "https://api.themoviedb.org/3/movie/now_playing?language=ko&page=1",
+    url: "https://api.themoviedb.org/3/movie/now_playing?language=ko",
     method: "GET",
     headers: {
       accept: "application/json",
-      Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwMDE2NjBkNjJhMDgyYjE4M2ZkODZiNjI4Y2M2OTAwNSIsIm5iZiI6MTcyMzk2NDg2MC40ODA1OTgsInN1YiI6IjYzNjIyODg1MDdlMjgxMDA3ZGI1YTFhYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.M93uRZZ6ZW_57LcK21DXpznrPRwRixqcALKI-2LGJwY",
+      Authorization: `${ApiInfo.key}`,
     },
   };
 
   $.ajax(settings).done(function (response) {
     console.log(response);
     //얻은 데이터를 바탕으로 배경화면 사진 설정
+    const base_url = ApiInfo.images.base_url;
+    const file_size = ApiInfo.images.backdrop_sizes.w780;
+    const img_url = response.results[random].backdrop_path;
     $(document).ready(function () {
+      console.log(base_url + img_url);
       $(".search-box").css({
-        "background-image":
-          'url("https://image.tmdb.org/t/p/w780/yDHYTfA3R0jFYba16jBB1ef8oIt.jpg")',
+        "background-image": `url(${base_url + file_size + img_url})`,
         "background-size": "cover",
         "background-position": "center",
         "background-repeat": "no-repeat",
